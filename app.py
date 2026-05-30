23405999e7a40658f854ba8b351e61c1d97ff564
abdullah <abdullahjalab06@gmail.com>
2026-05-30 16:40:31 +0300

    update

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def add_prod():
 
         add_product(productname, quantity, price)
 
-        return "Product Added"
+        return render_template("index.html")
 
     return render_template("add-product.html")
 
